f9cbd6ecea7ade6fadbd54e599c58c845e92a921
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-02-25 22:20:01 -0800

    csrf for RESTAPI

---
+++ src/main/java/com/spring/security/springsecurity/resources/SpringSecurityPlayResource.java
@@ -0,0 +1,14 @@
+package com.spring.security.springsecurity.resources;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringSecurityPlayResource {
+    @GetMapping("/csrf-token")
+    public CsrfToken retreiveCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+}
