e6f59046b72098bfe91a4031735d4230f0de0530
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-02-27 21:54:49 -0800

    Storing User Credentials in memory

--- src/main/java/com/spring/security/springsecurity/basic/BasicSecurityConfiguration.java
+++ src/main/java/com/spring/security/springsecurity/basic/BasicSecurityConfiguration.java
@@ -4,23 +4,40 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class BasicSecurityConfiguration {
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-
-        http.authorizeHttpRequests(auth ->{
-                    auth.anyRequest().authenticated();
-                });
-
-        http.sessionManagement(
-                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        );
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic();
         http.csrf().disable();
         return http.build();
+    }
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user = User.withUsername("smk").password("{noop}smk123").roles("USER").build();
+        var admin = User.withUsername("admin").password("{noop}mks123").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    // Define a separate configuration class for CORS
+    @Configuration
+    public static class CorsConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedMethods("*")
+                    .allowedOrigins("http://localhost:8080/");
+        }
     }
 }
