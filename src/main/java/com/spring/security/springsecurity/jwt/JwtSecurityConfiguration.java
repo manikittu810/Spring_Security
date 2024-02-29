15d272ad38e36d1805371ade4cdad42aa14014db
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-02-29 12:35:27 -0800

    Configuring decoder for JWT token

--- src/main/java/com/spring/security/springsecurity/jwt/JwtSecurityConfiguration.java
+++ src/main/java/com/spring/security/springsecurity/jwt/JwtSecurityConfiguration.java
@@ -1,5 +1,13 @@
 package com.spring.security.springsecurity.jwt;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.KeySourceException;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSelector;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.JWT;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -10,6 +18,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,7 +27,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.sql.DataSource;
-
+import com.nimbusds.jose.jwk.RSAKey;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPublicKey;
+import java.util.List;
+import java.util.UUID;
 import static org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION;
 
 @Configuration
@@ -79,8 +94,36 @@ public class JwtSecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    public JWTDecoder jwtDecoder(){
-//
-//    }
+    @Bean
+    public KeyPair keyPair(){
+        try{
+            var keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.generateKeyPair();
+        }
+        catch(Exception e){
+        throw new RuntimeException(e);
+        }
+    }
+    @Bean
+    public RSAKey rsaKey(KeyPair keyPair){
+        return new RSAKey
+                .Builder((RSAPublicKey)keyPair.getPublic())
+                .privateKey(keyPair.getPrivate())
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+    @Bean
+    public JWKSource jwkSource(RSAKey rsaKey){
+        var jwkSet = new JWKSet(rsaKey);
+        return (jwkSelector,context) -> jwkSelector.select(jwkSet);
+    }
+    @Bean
+    public JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException {
+        return NimbusJwtDecoder.withPublicKey(rsaKey.toRSAPublicKey())
+                .build();
+    }
+
+
+
 }
