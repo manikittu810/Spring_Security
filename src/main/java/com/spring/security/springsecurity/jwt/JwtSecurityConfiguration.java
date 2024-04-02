d3147b377c8c9fe070499b16ad5001e4b6d39eab
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-04-01 17:58:58 -0700

    Added JwtEncoder

--- src/main/java/com/spring/security/springsecurity/jwt/JwtSecurityConfiguration.java
+++ src/main/java/com/spring/security/springsecurity/jwt/JwtSecurityConfiguration.java
@@ -19,7 +19,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -124,6 +126,9 @@ public class JwtSecurityConfiguration {
                 .build();
     }
 
-
+    @Bean
+    public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwkSource) {
+        return new NimbusJwtEncoder(jwkSource);
+    }
 
 }
